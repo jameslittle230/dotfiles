08adc8dad35e8547fbe7a4c26aede5ce51e742dd
James Little <littleguy23@gmail.com>
2025-05-19 17:32:39 -0500

    Use 3rd party plugin for debugprint

--- home/.config/nvim/lua/jil/init.lua
+++ home/.config/nvim/lua/jil/init.lua
@@ -4,7 +4,6 @@ vim.g.maplocalleader = "\\"
 require("jil.settings")
 require("jil.terminal")
 require('jil.lazy')
-require('jil.log-variables').setup({})
 
 -- this one needs to be last
 require("jil.remaps")
--- home/.config/nvim/lua/plugins/comments.lua
+++ home/.config/nvim/lua/plugins/comments.lua
@@ -1,5 +1,4 @@
 return {
-  { 'JoosepAlviste/nvim-ts-context-commentstring' },
   {
     'numToStr/Comment.nvim',
     config = function()
@@ -10,4 +9,20 @@ return {
       }
     end,
   },
+  { 'JoosepAlviste/nvim-ts-context-commentstring' },
+  {
+    "andrewferrier/debugprint.nvim",
+    opts = {
+      display_location = true,
+      display_counter = false,
+      notify_for_registers = false,
+      highlight_lines = false,
+      print_tag = "JIL-LOG"
+    },
+    dependencies = {
+      "nvim-telescope/telescope.nvim", -- Optional: If you want to use the :SearchDebugPrints command with telescope.nvim
+    },
+    lazy = false,                      -- Required to make line highlighting work before debugprint is first used
+    version = "*",                     -- Remove if you DON'T want to use the stable version
+  }
 }
