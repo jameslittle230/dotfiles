223d73a217ef351cd9ee00e39037e3d26ccbb113
James Little <littleguy23@gmail.com>
2026-03-31 08:35:32 -0400

    Improve conform integration

--- home/.config/nvim12/init.lua
+++ home/.config/nvim12/init.lua
@@ -229,7 +229,7 @@ require("conform").setup({
     css = { "prettierd", "prettier", stop_after_first = true },
     html = { "prettierd", "prettier", stop_after_first = true },
     markdown = { "prettierd", "prettier", stop_after_first = true },
-    lua = { "stylua" },
+    lua = { "stylua", lsp_format = "never" },
     python = { "black" },
     rust = { "rustfmt", lsp_format = "fallback" },
   },
@@ -239,16 +239,30 @@ require("conform").setup({
     },
   },
   format_on_save = function(bufnr)
-    local filetype = vim.bo[bufnr].filetype
-    if filetype == "yaml" or filetype == "yml" then
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
       return
     end
-
-    return {
-      timeout_ms = 500,
-      lsp_fallback = true, -- Uses LSP if no conform formatter configured
-    }
+    return { timeout_ms = 500, lsp_fallback = false }
   end,
+})
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
 })
 
 local lsp_servers = {
